2e8023c898b57e5784a52b9acbb497c60692e2fa
Meemom <jemima.silaen@gmail.com>
2025-06-21 23:43:48 +0700

    chatbox component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function App() {
+    const [text, setText] = useState('');
+    const [result, setResult] = useState(null);
+
+    return (
+        <div>
+            <h1>Polar Bear</h1>
+            
+        </div>
+    )
+
+}
